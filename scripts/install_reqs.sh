6fca69994f544265e2e3993029b29889df99a59f
Kristine Kalaw <kristine1k@ai-work.com>
2023-12-21 14:06:14 +0800

    chore: Create convenience scripts for app startup/shutdown

---
+++ scripts/install_reqs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NC="\033[0m" # No Color
+GREEN="\033[0;32m"
+LIGHT_GREEN="\033[1;32m"
+
+#----------
+echo -e "${GREEN}Installs the python packages${NC}"
+echo -e "${LIGHT_GREEN}$ pip install -r requirements.txt${NC}"
+pip install -r requirements.txt
+echo ""
+
+
